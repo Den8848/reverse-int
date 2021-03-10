f03f7265ef6002d310dd9934dd589584c6d9a022
den8848 <denis2016wolverine2016@gmail.com>
2021-03-11 00:14:30 +0200

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+        let convert = n.toString()
+        let splitNum = convert.split("")
+        let reverseNum = splitNum.reverse()
+        let newInt = reverseNum.join("")
+        // let result = newInt.parseInt(joinNum)
+        return newInt
 }
